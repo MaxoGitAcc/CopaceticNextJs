a619068b00c9783c54be4be4cf967f2b0e7ad925
MaxoGitAcc <maxowork19@gmail.com>
2026-02-02 18:01:48 +0400

    Cleaned up

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,31 +1,9 @@
-"use client";
+import React from 'react'
 
-import type { ThemeProviderProps } from "next-themes";
-
-import * as React from "react";
-import { HeroUIProvider } from "@heroui/system";
-import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export interface ProvidersProps {
-  children: React.ReactNode;
-  themeProps?: ThemeProviderProps;
-}
-
-declare module "@react-types/shared" {
-  interface RouterConfig {
-    routerOptions: NonNullable<
-      Parameters<ReturnType<typeof useRouter>["push"]>[1]
-    >;
-  }
-}
-
-export function Providers({ children, themeProps }: ProvidersProps) {
-  const router = useRouter();
-
+const providers = () => {
   return (
-    <HeroUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </HeroUIProvider>
-  );
+    <div>providers</div>
+  )
 }
+
+export default providers
